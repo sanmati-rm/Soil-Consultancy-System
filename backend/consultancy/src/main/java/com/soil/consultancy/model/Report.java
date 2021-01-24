87b2b810fbd8e7d448e86940ef53365c383ffcbb
Sanmati RM <46448758+sanmati-rm@users.noreply.github.com>
2021-01-24 20:19:53 +0530

    Added Backend

---
+++ backend/consultancy/src/main/java/com/soil/consultancy/model/Report.java
@@ -0,0 +1,65 @@
+package com.soil.consultancy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.NotNull;
+import java.lang.Float;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Report {
+
+    @Id
+    private UUID reportId;
+
+    @NotNull
+    private Float nitrogen;
+
+    @NotNull
+    private Float phosphor;
+
+    @NotNull
+    private Float  potassium;
+
+    @NotNull
+    private Float calcium;
+
+    @NotNull
+    private  Float magnesium;
+
+    @NotNull
+    private Float sulphur;
+
+    @NotNull
+    private Float iron;
+
+    @NotNull
+    private Float zinc;
+
+    @NotNull
+    private Float manganese;
+
+    @NotNull
+    private Float copper;
+
+    @NotNull
+    private Float molybdenum;
+
+    @NotNull
+    private Float boron;
+
+    @NotNull
+    private Float chlorine;
+
+    @NotNull
+    private Float nickel;
+
+    @NotNull
+    private String crop;
+}
+
